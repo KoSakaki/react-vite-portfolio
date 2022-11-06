92d31d266ffb42a62b2462356f123267dd5c50d6
KoSakaki <sakaki@flaginc.net>
2022-11-06 14:39:56 +0900

    future: Values

--- src/components/items/Values.tsx
+++ src/components/items/Values.tsx
@@ -3,9 +3,12 @@
 export const Values = () => {
   return (
     <>
-      <h1 className="flex justify-center">
-        価値観
-      </h1>
+      <h1 className="flex justify-center">Values</h1>
+      <div className="flex justify-center text-xl w-10/12">
+        <div>
+          仮説ドリブンで物事を考えます。とにかくコードを書き、動かすことで論理を組み立てます。思考回路は、「こうなればこうなるということは、これもこうなるだろう。だからやってみる。違ったらまた考えれば良いさ。」です。
+        </div>
+      </div>
     </>
   )
 }
