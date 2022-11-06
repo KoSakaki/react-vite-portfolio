95e3a1729aa4c7a5e64f436ae4aa731fef1ef729
KoSakaki <sakaki@flaginc.net>
2022-11-06 12:12:12 +0900

    forth commit

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,20 +1,22 @@
 import face from '../assets/imagef.png';
 
 export const Head = () => {
-  const divStyle:React.CSSProperties = {
-    color: 'blue',
-    position: 'absolute',
-    left: '50%',
-    bottom: '5%',
-  };
-  const faceImage:React.CSSProperties = {
-    borderRadius: '50% 50%',
+  const style: {[key: string]: React.CSSProperties} = {
+    divStyle: {
+      color: 'blue',
+      position: 'absolute',
+      left: '50%',
+      bottom: '5%',
+    },
+    faceImage: {
+       borderRadius: '50% 50%',
+    }
   }
 
   return (
     <div className="text-3xl font-bold underline flex justify-center relative">
-      <img src={face} style={faceImage} />
-      <p className="divStyle" style={divStyle}>Koya Sakaki</p>
+      <img src={face} style={style.faceImage} />
+      <p className="divStyle" style={style.divStyle}>Koya Sakaki</p>
     </div>
   )
 
